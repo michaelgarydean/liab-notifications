bd0533c33f81a6f6cfb249f9e368f7e551965c4d
Michael Dean <michaeldean@Michaels-MacBook-Pro.local>
2022-06-11 18:29:03 -0400

    working notifications each 20-30 seconds up to a total of 64 notifications

---
+++ Source/notify/notifyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "notifyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOTIFY_API AnotifyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
